6231748147b2b8696c5ac827ae14ef59870e0916
whiochon <oliver_white@yahoo.com>
2022-09-07 13:10:22 +0000

    Added s3 bucket module 3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/Lurking-Benevolence/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket_prefix = "oli"
   acl    = "private"
 
   versioning = {
