d4ad68960a691276882b05f9b47874ad1c27887e
Oliver White <97488371+whiochon@users.noreply.github.com>
2022-09-07 22:12:50 +1000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. You can't have egg, bacon, Spam and sausage without the Spam!  Beans.
   <!-- END -->
 
   </div>
